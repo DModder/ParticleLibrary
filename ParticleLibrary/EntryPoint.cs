a385d76fff167fe8485b7387e430fc8859b0c55b
DModder <dmodder@anapps.dk>
2015-07-08 20:01:53 +0200

    Uploaded whole project

---
+++ ParticleLibrary/EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿[assembly: Rage.Attributes.Plugin("Particle Library", Description = "Used for spawning particles with RAGE Plugin Hook", Author = "DModder")]
+
+namespace ParticleLibrary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Rage;
+
+    public class EntryPoint
+    {
+        public static void Main()
+        {
+            //Keeping the plugin alive and looping
+            while (true)
+            {
+
+            }
+        }
+    }
+}
---
+++ ParticleLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ParticleLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ParticleLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3ad4a9cd-b735-434f-ad32-bf2ff6e3e7af")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
